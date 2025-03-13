ca82b8ab2f0647003c4dd433433fc1eeec657a6f
unknown <ce.rumiakter@gmail.com>
2025-03-13 11:46:38 +0600

    add sale flat section

--- src/components/SaleFlats/SaleFlats.jsx
+++ src/components/SaleFlats/SaleFlats.jsx
@@ -1,10 +1,30 @@
+import { useEffect, useState } from "react";
 import SaleBanner from "./SaleBanner";
+import SaleFlat from "./SaleFlat";
 
 
 const SaleFlats = () => {
+    const [saleFlats, setSaleFlats] = useState([]);
+
+    useEffect(() => {
+        fetch('flats.json')
+            .then(res => res.json())
+            .then(data => setSaleFlats(data))
+    }, [])
     return (
         <div>
             <SaleBanner></SaleBanner>
+            <div className="mt-14 mb-14 container mx-auto">
+                <div className="flex justify-between items-center mb-6">
+                    <h1 className="text-2xl font-medium">Flat For Sale</h1>
+                    <p>See all</p>
+                </div>
+                <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-8">
+                    {
+                        saleFlats.map(saleFlat => <SaleFlat saleFlat={saleFlat}></SaleFlat>)
+                    }
+                </div>
+            </div>
         </div>
     );
 };
