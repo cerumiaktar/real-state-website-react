1f9b859cf16696e943f19488bbfbc02f8f397cbb
unknown <ce.rumiakter@gmail.com>
2025-09-18 11:55:59 +0600

    update blog section

--- src/components/AllBlogs/AllBlog.jsx
+++ src/components/AllBlogs/AllBlog.jsx
@@ -3,17 +3,17 @@
 const AllBlog = ({ allBlog }) => {
     const{img,title,description} = allBlog;
     return (
-        <div className="card bg-base-100 shadow-sm">
+        <div className="card bg-base-100 dark:bg-white shadow-sm">
             <figure>
                 <img
                     src={img}
                     alt="Shoes" />
             </figure>
             <div className="card-body">
-                <h2 className="card-title">{title}</h2>
-                <p className="text-[#414141] leading-6">{description}</p>
+                <h2 className="card-title dark:text-black">{title}</h2>
+                <p className="text-[#414141] dark:text-[#333333] leading-6">{description}</p>
                 <div className="card-actions">
-                    <button className="btn bg-[#B4CB21] w-full">Read More</button>
+                    <button className="btn bg-[#B4CB21] w-full dark:border-none">Read More</button>
                 </div>
             </div>
         </div>
