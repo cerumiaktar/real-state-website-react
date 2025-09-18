1ec04e6caefe983c0c5a8d6a46b5702b2f526e83
unknown <ce.rumiakter@gmail.com>
2025-09-18 12:10:26 +0600

    update all

--- src/components/Login/Login.jsx
+++ src/components/Login/Login.jsx
@@ -5,20 +5,20 @@ const Login = () => {
     return (
         <div className="container mx-auto">
             <div className="border-2 border-gray-300 rounded-2xl mt-8 mb-8 px-11 py-11 space-y-4">
-                <h1 className="text-2xl font-medium text-center">Login Now</h1>
+                <h1 className="text-2xl font-medium text-center dark:text-black">Login Now</h1>
                 <fieldset className="fieldset">
-                    <legend className="fieldset-legend text-xl">Email</legend>
-                    <input type="email" className="input w-full" placeholder="Enter Your Email" />
+                    <legend className="fieldset-legend text-xl dark:text-black">Email</legend>
+                    <input type="email" className="input w-full dark:bg-white dark:text-[#333333] dark:border-[#cacaca]" placeholder="Enter Your Email" />
                 </fieldset>
                 <fieldset className="fieldset">
-                    <legend className="fieldset-legend text-xl">Password</legend>
-                    <input type="password" className="input w-full" placeholder="Enter Your Password" />
+                    <legend className="fieldset-legend text-xl dark:text-black">Password</legend>
+                    <input type="password" className="input w-full dark:bg-white dark:text-[#333333] dark:border-[#cacaca]" placeholder="Enter Your Password" />
                 </fieldset>
                 <div className="flex items-center justify-end">
                     <p className="text-red-600">Forgot Password</p>
                 </div>
-                <button className="btn w-full text-center bg-[#637307] text-white text-xl">Login</button>
-                <p className="text-center">Don’t have an account <Link to='/register' className="text-[#637307] font-medium">Register</Link></p>
+                <button className="btn w-full text-center bg-[#637307] text-white text-xl dark:border-none">Login</button>
+                <p className="text-center dark:text-[#333333]">Don’t have an account <Link to='/register' className="text-[#637307] font-medium">Register</Link></p>
             </div>
             
         </div>
--- src/components/Register/Register.jsx
+++ src/components/Register/Register.jsx
@@ -5,29 +5,29 @@ const Register = () => {
     return (
         <div className="container mx-auto">
             <div className="border-2 border-gray-300 rounded-2xl mt-8 mb-8 px-11 py-11 space-y-4">
-                <h1 className="text-2xl font-medium text-center">Register Now</h1>
+                <h1 className="text-2xl font-medium text-center dark:text-black">Register Now</h1>
                 <fieldset className="fieldset">
-                    <legend className="fieldset-legend text-xl">Name</legend>
-                    <input type="text" className="input w-full" placeholder="Enter Your Name" />
+                    <legend className="fieldset-legend text-xl dark:text-black">Name</legend>
+                    <input type="text" className="input w-full dark:bg-white dark:text-[#333333] dark:border-[#cacaca]" placeholder="Enter Your Name" />
                 </fieldset>
                 <fieldset className="fieldset">
-                    <legend className="fieldset-legend text-xl">Phone Number</legend>
-                    <input type="text" className="input w-full" placeholder="Enter Your Phone Number" />
+                    <legend className="fieldset-legend text-xl dark:text-black">Phone Number</legend>
+                    <input type="text" className="input w-full dark:bg-white dark:text-[#333333] dark:border-[#cacaca]" placeholder="Enter Your Phone Number" />
                 </fieldset>
                 <fieldset className="fieldset">
-                    <legend className="fieldset-legend text-xl">Email</legend>
-                    <input type="email" className="input w-full" placeholder="Enter Your Email" />
+                    <legend className="fieldset-legend text-xl dark:text-black">Email</legend>
+                    <input type="email" className="input w-full dark:bg-white dark:text-[#333333] dark:border-[#cacaca]" placeholder="Enter Your Email" />
                 </fieldset>
                 <fieldset className="fieldset">
-                    <legend className="fieldset-legend text-xl">Password</legend>
-                    <input type="password" className="input w-full" placeholder="Enter Your Password" />
+                    <legend className="fieldset-legend text-xl dark:text-black">Password</legend>
+                    <input type="password" className="input w-full dark:bg-white dark:text-[#333333] dark:border-[#cacaca]" placeholder="Enter Your Password" />
                 </fieldset>
                 <div className="flex items-center">
                     <input type="checkbox" className="checkbox checkbox-sm" />
                     <p className="text-[#585757]">Agree with <Link className="font-medium text-[#637307]" to='/terms'>terms & condition</Link></p>
                 </div>
-                <button className="btn w-full text-center bg-[#637307] text-white text-xl">Register</button>
-                <p className="text-center">Don’t have an account <Link to='/login' className="text-[#637307] font-medium">Login</Link></p>
+                <button className="btn w-full text-center bg-[#637307] text-white text-xl dark:border-none">Register</button>
+                <p className="text-center dark:text-[#333333]">Don’t have an account <Link to='/login' className="text-[#637307] font-medium">Login</Link></p>
             </div>
 
         </div>
