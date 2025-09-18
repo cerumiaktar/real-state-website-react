ba7df67f8158499b58164d2e48b98997e412526d
unknown <ce.rumiakter@gmail.com>
2025-09-18 11:25:32 +0600

    update expert section

--- src/components/Experts/Expert.jsx
+++ src/components/Experts/Expert.jsx
@@ -4,17 +4,17 @@ const Expert = ({ expert }) => {
     const { img, name, designation } = expert;
     return (
         <div>
-            <div className="card bg-base-100 shadow-sm">
+            <div className="card bg-base-100 dark:bg-white shadow-sm">
                 <figure>
                     <img
                         src={img}
                         alt="Shoes" />
                 </figure>
-                <div className="card-body text-center">
+                <div className="card-body text-center dark:text-black">
                     <h2 className="card-title mx-auto">{name}</h2>
                     <p>{designation}</p>
                     <div className="card-actions ">
-                        <button className="btn bg-[#A4BC14] w-full">View Profile</button>
+                        <button className="btn bg-[#A4BC14] w-full dark:border-none">View Profile</button>
                     </div>
                 </div>
             </div>
