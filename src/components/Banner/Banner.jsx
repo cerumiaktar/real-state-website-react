5a04caebc8aba13f60c7cde6a3faa70a0681b855
unknown <ce.rumiakter@gmail.com>
2025-09-18 11:09:37 +0600

    update banner

--- src/components/Banner/Banner.jsx
+++ src/components/Banner/Banner.jsx
@@ -16,22 +16,22 @@ const Banner = () => {
                             quiet suburban retreat.
                         </p>
                         <div className="space-x-8">
-                            <button className="btn bg-white">All</button>
+                            <button className="btn bg-white dark:border-none dark:text-[#637307]">All</button>
                             <Link to='/sale'><button className="btn btn-outline">For Sale</button></Link>
                             <Link to='/rent'><button className="btn btn-outline">For Rent</button></Link>
                         </div>
                         <div className="bg-white mt-8 py-4 flex flex-col md:flex-row items-center justify-center gap-8 rounded-lg">
                             <div>
-                                <button className="text-[#637307] flex items-center btn border-[#637307]" ><span><HiOutlineLocationMarker /></span>City</button>
+                                <button className="text-[#637307] flex items-center btn border-[#637307] dark:bg-white" ><span><HiOutlineLocationMarker /></span>City</button>
                             </div>
                             <div>
-                                <button className="text-[#637307] flex items-center btn border-[#637307]" >Property type<span><MdOutlineKeyboardArrowDown /></span></button>
+                                <button className="text-[#637307] flex items-center btn border-[#637307] dark:bg-white" >Property type<span><MdOutlineKeyboardArrowDown /></span></button>
                             </div>
                             <div>
-                                <button className="text-[#637307] flex items-center btn border-[#637307]" >Price<span><MdOutlineKeyboardArrowDown /></span></button>
+                                <button className="text-[#637307] flex items-center btn border-[#637307] dark:bg-white" >Price<span><MdOutlineKeyboardArrowDown /></span></button>
                             </div>
                             <div>
-                                <button className="text-white flex items-center btn bg-[#637307]" ><span><RiSearchLine /></span>Search</button>
+                                <button className="text-white flex items-center btn bg-[#637307] dark:border-none" ><span><RiSearchLine /></span>Search</button>
                             </div>
                         </div>
                     </div>
