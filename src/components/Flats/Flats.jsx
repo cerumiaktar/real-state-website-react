ea67fd9eae5de11c783bad0ace2df1e8d7d47d3a
unknown <ce.rumiakter@gmail.com>
2025-09-18 11:13:10 +0600

    update featured product section

--- src/components/Flats/Flats.jsx
+++ src/components/Flats/Flats.jsx
@@ -15,8 +15,8 @@ const Flats = () => {
     return (
         <div className="container mx-auto mt-14 mb-14">
             <div className="flex justify-between items-center mb-6">
-                <h1 className="text-2xl font-medium">Our Featured Flat</h1>
-                <p>See all</p>
+                <h1 className="text-2xl font-medium dark:text-black">Our Featured Flat</h1>
+                <p className="dark:text-[#333333]">See all</p>
             </div>
             <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-8">
                 {
@@ -24,7 +24,7 @@ const Flats = () => {
                 }
             </div>
             <div className="mt-6 flex justify-center">
-                <button className="btn border-[#637307] text-[#637307] text-lg">Load More</button>
+                <button className="btn border-[#637307] text-[#637307] text-lg dark:bg-white">Load More</button>
             </div>
         </div>
     );
