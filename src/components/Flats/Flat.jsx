1ae9ba2e7cd2cc4e394936a3e194f6e5d76470ec
unknown <ce.rumiakter@gmail.com>
2025-10-30 13:45:18 +0600

    update

--- src/components/Flats/Flat.jsx
+++ src/components/Flats/Flat.jsx
@@ -72,11 +72,27 @@ const Flat = ({ flat }) => {
                         src={img}
                         alt="Shoes" />
                 </figure>
-                <div className="card-body">
-                    <h2 className="card-title">{title}</h2>
+                <div className="card-body text-center">
+                    <h2 className="card-title mx-auto">{title}</h2>
+                    <p className="text-lg font-medium text-[#637307]">{price}</p>
                     <p>{category}</p>
-                    <div className="card-actions justify-end">
-                        <button className="btn btn-primary">Buy Now</button>
+                    <div className="flex items-center justify-center gap-8 ">
+                        <div className="flex items-center gap-2 text-lg font-medium">
+                            <p><LiaBathSolid /></p>
+                            <p>4</p>
+                        </div>
+                        <div className="flex items-center gap-2 text-lg font-medium">
+                            <p><IoBedOutline /></p>
+                            <p>3</p>
+                        </div>
+                        <div className="flex items-center gap-2 text-lg font-medium">
+                            <p><IoCarOutline /></p>
+                            <p>1</p>
+                        </div>
+                        <div className="flex items-center gap-2 text-lg font-medium">
+                            <p><IoHomeOutline /></p>
+                            <p>1200 m<sup>2</sup> </p>
+                        </div>
                     </div>
                 </div>
             </div>
