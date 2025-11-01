ddaa0dcd02e6c700a3a597461b930c9c4482b576
unknown <ce.rumiakter@gmail.com>
2025-11-01 15:56:51 +0600

    update saleflat

--- src/components/RentFlats/RentFlat.jsx
+++ src/components/RentFlats/RentFlat.jsx
@@ -6,35 +6,35 @@ const RentFlat = ({ rentFlat }) => {
     const { img, title, price, category } = rentFlat;
     return (
         <div className="card bg-base-100 dark:bg-white dark:text-black shadow-sm">
-                <figure>
-                    <img
-                        src={img}
-                        alt="Shoes" />
-                </figure>
-                <div className="card-body text-center">
-                    <h2 className="card-title mx-auto">{title}</h2>
-                    <p className="text-lg font-medium text-[#637307]">{price}</p>
-                    <p>{category}</p>
-                    <div className="flex items-center justify-center gap-8 ">
-                        <div className="flex items-center gap-2 text-lg font-medium">
-                            <p><LiaBathSolid /></p>
-                            <p>4</p>
-                        </div>
-                        <div className="flex items-center gap-2 text-lg font-medium">
-                            <p><IoBedOutline /></p>
-                            <p>3</p>
-                        </div>
-                        <div className="flex items-center gap-2 text-lg font-medium">
-                            <p><IoCarOutline /></p>
-                            <p>1</p>
-                        </div>
-                        <div className="flex items-center gap-2 text-lg font-medium">
-                            <p><IoHomeOutline /></p>
-                            <p>1200 m<sup>2</sup> </p>
-                        </div>
+            <figure>
+                <img
+                    src={img}
+                    alt="Shoes" />
+            </figure>
+            <div className="card-body text-center">
+                <h2 className="card-title mx-auto">{title}</h2>
+                <p className="text-lg font-medium text-[#637307]">{price}</p>
+                <p>{category}</p>
+                <div className="flex items-center justify-center gap-8 ">
+                    <div className="flex items-center gap-2 text-lg font-medium">
+                        <p><LiaBathSolid /></p>
+                        <p>4</p>
+                    </div>
+                    <div className="flex items-center gap-2 text-lg font-medium">
+                        <p><IoBedOutline /></p>
+                        <p>3</p>
+                    </div>
+                    <div className="flex items-center gap-2 text-lg font-medium">
+                        <p><IoCarOutline /></p>
+                        <p>1</p>
+                    </div>
+                    <div className="flex items-center gap-2 text-lg font-medium">
+                        <p><IoHomeOutline /></p>
+                        <p>1200 m<sup>2</sup> </p>
                     </div>
                 </div>
             </div>
+        </div>
     );
 };
 
--- src/components/SaleFlats/SaleFlat.jsx
+++ src/components/SaleFlats/SaleFlat.jsx
@@ -5,41 +5,11 @@ const SaleFlat = ({ saleFlat }) => {
     const { img, title, price, category } = saleFlat;
     return (
         <div className="card bg-base-100 dark:bg-white dark:text-black shadow-sm">
-            <figure className="relative">
-                <div className="carousel rounded-box h-[287px]">
-                    <div className="carousel-item w-full ">
-                        <img
-                            src={img}
-                            className="w-full"
-                            alt="Tailwind CSS Carousel component" />
-                    </div>
-                    <div className="carousel-item w-full">
-                        <img
-                            src="https://i.ibb.co.com/Kx7M3GZR/image-3.png"
-                            className="w-full"
-                            alt="Tailwind CSS Carousel component" />
-                    </div>
-                    <div className="carousel-item w-full">
-                        <img
-                            src="https://i.ibb.co.com/ksfFYMXn/image-4.png"
-                            className="w-full"
-                            alt="Tailwind CSS Carousel component" />
-                    </div>
-                    <div className="carousel-item w-full">
-                        <img
-                            src="https://i.ibb.co.com/qL1wKqCG/image-5.png"
-                            className="w-full"
-                            alt="Tailwind CSS Carousel component" />
-                    </div>
-                </div>
-                <div className="flex gap-2 justify-center items-center absolute inset-x-0 bottom-4">
-                    <p className="bg-[#637307] w-3 h-3 rounded-full"></p>
-                    <p className="bg-[#D9D9D9] w-3 h-3 rounded-full"></p>
-                    <p className="bg-[#D9D9D9] w-3 h-3 rounded-full"></p>
-                    <p className="bg-[#D9D9D9] w-3 h-3 rounded-full"></p>
-                </div>
+            <figure>
+                <img
+                    src={img}
+                    alt="Shoes" />
             </figure>
-
             <div className="card-body text-center">
                 <h2 className="card-title mx-auto">{title}</h2>
                 <p className="text-lg font-medium text-[#637307]">{price}</p>
@@ -63,7 +33,6 @@ const SaleFlat = ({ saleFlat }) => {
                     </div>
                 </div>
             </div>
-
         </div>
     );
 };
